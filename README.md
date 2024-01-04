eee86e849f9212ade3acae346beaeeb1f36d5973
Chiao-Han Chang <mandy93142@gmail.com>
2024-01-03 18:37:47 -0800

    modified

--- README.md
+++ README.md
@@ -1,29 +1,19 @@
 # module-04-challenge
 
-* Link to Website: 
+Link to Website: 
 
 Web APIs Challenge: Code Quiz
 
 This module’s Challenge invites you to build a timed coding quiz with multiple-choice questions. This app will run in the browser, and will feature dynamically updated HTML and CSS powered by JavaScript code that you write. It will have a clean, polished, and responsive user interface. Below is the acceptance criteria:
 
 GIVEN I am taking a code quiz
-
 WHEN I click the start button
-
 THEN a timer starts and I am presented with a question
-
 WHEN I answer a question
-
 THEN I am presented with another question
-
 WHEN I answer a question incorrectly
-
 THEN time is subtracted from the clock
-
 WHEN all questions are answered or the timer reaches 0
-
 THEN the game is over
-
 WHEN the game is over
-
 THEN I can save my initials and score
--- assets/js/script.js
+++ assets/js/script.js
@@ -28,6 +28,38 @@ const questions = [
             { text: 'parenthesis', correct: false },
             { text: 'square brakets', correct: false }
         ]
+    },
+
+    {
+        question: 'Arrays in JavaScript can be used to store:', 
+        answers: [
+            { text: 'numbers and strings', correct: false },
+            { text: 'other arrays', correct: false },
+            { text: 'booleans', correct: false },
+            { text: 'all of the above', correct: true }
+        ]
+    },
+
+    {
+        question: 'String values must be enclosed within ( ) when being assigned to variables.', 
+        answers: [
+            { text: 'commas', correct: false },
+            { text: 'curly brackets', correct: false },
+            { text: 'quotes', correct: true },
+            { text: 'parenthesis', correct: false },
+        ]
+
+    },
+
+    {
+        question: 'A very useful tool used during development and debugging for printing content to the debugger is:', 
+        answers: [
+            { text: 'JavaScript', correct: false },
+            { text: 'terminal/bash', correct: false },
+            { text: 'for loops', correct: false },
+            { text: 'console.log', correct: true },
+        ]
+
     }
     
 ];
